95fbdbd3180f02608203e933d3de9e8905079391
Peterturn <55192090+Peterturn@users.noreply.github.com>
2021-06-17 12:47:43 +0200

    adjusted some styling

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -27,31 +27,29 @@
             </div>
         </div>
     </nav>
-    <div class="container mt-3">
-
-
+    <div class="container my-5">
 
         <?php echo $contenuDeLaPage ?>
 
-
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="index.php">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarColor01">
-                    <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="index.php">Home
-                                <span class="visually-hidden">(current)</span>
-                            </a>
-                        </li>
-                </div>
-            </div>
-        </nav>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
     </div>
+
+    <nav style="position: fixed; left: 0; bottom: 0; width: 100%" class="navbar navbar-expand-lg navbar-dark bg-dark mt-5">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="index.php">Home
+                            <span class="visually-hidden">(current)</span>
+                        </a>
+                    </li>
+            </div>
+        </div>
+    </nav>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 
 </html>
